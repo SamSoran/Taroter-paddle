67768d76d8c1035acd4aab94c0dfa2029d032a91
SamSoran <samuel.v.soriano@gmail.com>
2024-09-27 17:55:51 +0800

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,15 @@
-import { type NextRequest } from 'next/server';
-import { updateSession } from '@/utils/supabase/middleware';
+import { type NextRequest, NextResponse } from 'next/server';
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req: request, res });
+
+  await supabase.auth.getSession();
+
+  return res;
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    '/((?!_next/static|_next/image|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
 };
